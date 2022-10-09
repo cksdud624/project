7c7d27c77e08994085d850295ca4bbfe172cb8a8
HCY <cksdud624@gmail.com>
2022-10-09 17:54:31 +0900

    1

--- mainserver/views.py
+++ mainserver/views.py
@@ -30,8 +30,6 @@ def tokentest(request):
     creds = None
     if os.path.exists('token.json'):
         creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    output = run("pwd", capture_output=True).stdout
-    return HttpResponse(output)
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
@@ -40,6 +38,8 @@ def tokentest(request):
             creds = flow.run_local_server()
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
+    output = run("echo", capture_output=True).stdout
+    return HttpResponse(output)
 
 
 @csrf_exempt
