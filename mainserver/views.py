cdfe98004442d0b39ffc8a97a41d265ff03ecc6e
HCY <cksdud624@gmail.com>
2022-10-31 22:18:42 +0900

    oh

--- mainserver/views.py
+++ mainserver/views.py
@@ -45,6 +45,7 @@ def test2(request):
 def test(request):
     if request.method == "POST":
         data = json.loads(request.body.decode('utf-8'))
+        #일정 생성
         if data['commandtype'] == "insertschedule":
             authcheck = usertable.objects.filter(userID=data['userid'], groupID=data['groupID'], caltype=data['caltype'])
             scheduleinsert = scheduletable(userID=data['userid'], caltype=data['caltype'], groupID=data['groupID'],
@@ -52,6 +53,7 @@ def test(request):
                                            month=data['month'], day=data['day'],
                                            hour=data['hour'], minute=data['minute'],
                                            content=data['content'])
+            #일정 리스트화
             queryset = authcheck.values_list()
             authquery = queryset[0][5]
             if authquery == "2" or authquery == "3":
@@ -59,7 +61,9 @@ def test(request):
                 return HttpResponse("insert complete")
             else:
                 return HttpResponse("Authority error")
+        #사용자ID 생성
         elif data['commandtype'] == "createid":
+            #사용자ID 조회
             useridcheck = usertable.objects.filter(userID=data['userid'])
             if useridcheck.count() == 0:
                 createid = usertable(userID=data['userid'], caltype="Public", password=data['password'], groupID=1,
@@ -68,24 +72,34 @@ def test(request):
                 return HttpResponse("ID created")
             else:
                 return HttpResponse("ID already exists")
+        #사용자ID 변경
         elif data['commandtype'] == "changeid":
+            #사용자ID 및 패스워드 조회
             useridcheck = usertable.objects.filter(userID=data['userid'], password=data['password'])
             if useridcheck.count() == 0:
                 return HttpResponse("ID not exists or Password error")
             else:
                 return HttpResponse("ID exists")
+        #사용자ID 삭제
         elif data['commandtype'] == "deleteid":
+            #사용자ID 조회
             useridcheck = usertable.objects.filter(userID=data['userid'])
+            #패스워드 검사
             passwordcheck = usertable.objects.filter(userID=data['userid'], password=data['password'], caltype="Public")
+            #사용자의 퍼블릭 캘린더 일정 삭제
+            deletealldate = scheduletable.objects.filter(userID=data['userid'], caltype="Public")
             if useridcheck.count() >= 1:
                 if passwordcheck.count() >= 1:
                     useridcheck.delete()
+                    deletealldate.delete()
                     return HttpResponse("ID deleted")
                 else:
                     return HttpResponse("Password error")
             else:
                 return HttpResponse("ID not exists")
+        #캘린더ID생성
         elif data['commandtype'] == "groupIDcreate":
+            #캘린더ID 조회
             groupidcheck = usertable.objects.filter(groupID=data['groupID'])
             if groupidcheck.count() == 0:
                 groupIDcreate = usertable(userID=data['userid'], caltype="Group", grouppassword=data['password'],
@@ -94,25 +108,34 @@ def test(request):
                 return HttpResponse("GroupID created")
             else:
                 return HttpResponse("GroupID already exists")
+        #캘린더ID변경
         elif data['commandtype'] == "groupIDchange":
+            #캘린더ID 및 패스워드 조회
             groupidcheck = usertable.objects.filter(groupID=data['groupID'], caltype="Group",
                                                     grouppassword=data['password'])
+            #사용자의 캘린더 등록 여부
             usergroupidcheck = usertable.objects.filter(groupID=data['groupID'], caltype="Group", userID=data['userid'])
             if groupidcheck.count() == 0:
                 return HttpResponse("ID not exists or Password error")
             else:
                 if usergroupidcheck.count() == 0:
+                    #사용자가 캘린더에 등록되어 있지 않으면 등록
                     groupIDcreate = usertable(userID=data['userid'], caltype="Group",
                                               groupID=data['groupID'], authrank="1")
                     groupIDcreate.save()
                 return HttpResponse("GroupID exists")
-
+        #캘린더ID삭제
         elif data['commandtype'] == "groupIDdelete":
+            #캘린더 조회
             groupidcheck = usertable.objects.filter(groupID=data['groupID'], caltype="Group")
+            #캘린더 권한 조회
             groupauthcheck = usertable.objects.filter(groupID=data['groupID'], caltype="Group",
                                                       userID=data['userid'], authrank="3")
+            #캘린더 패스워드 조회
             grouppasswordcheck = usertable.objects.filter(groupID=data['groupID'], caltype="Group",
                                                           grouppassword=data['password'])
+            #삭제된 캘린더의 일정 삭제
+            groupscheduledelete = scheduletable.objects.filter(groupId=data['groupID'], caltype="Group")
             if groupidcheck.count() == 0:
                 return HttpResponse("GroupID not exists")
             else:
@@ -122,10 +145,14 @@ def test(request):
                     if grouppasswordcheck.count() == 0:
                         return HttpResponse("GroupPassword error")
                     else:
+                        groupscheduledelete.delete()
                         groupidcheck.delete()
                         return HttpResponse("GroupID deleted")
+        #그룹 캘린더 관리 화면 사용자 조회
         elif data['commandtype'] == "searchcurrentgroup":
+            #그룹 캘린더 사용자 조회
             groupidcheck = usertable.objects.filter(userID=data['userid'], groupID=data['groupID'], caltype="Group")
+            #사용자 권환 조회
             groupauthcheck = usertable.objects.filter(groupID=data['groupID'], caltype="Group",
                                                       userID=data['userid'], authrank="3")
             if groupidcheck.count() == 0:
@@ -139,6 +166,7 @@ def test(request):
                     conn.execute('SELECT userID, authrank from mainserver_usertable WHERE caltype = :group AND '
                                  'groupID = :groupID', {"group": 'Group', "groupID": data['groupID']})
                     jsonfile = []
+                    #사용자들의 정보를 JSON파일로 응답
                     for i in conn.fetchall():
                         jsonfile.append({
                                 "userid": i[0],
@@ -146,8 +174,11 @@ def test(request):
                         })
                         jsonparse = json.dumps({'items': jsonfile})
                     return HttpResponse(jsonparse)
+        #그룹 캘린더 관리 화면 권한 변경
         elif data['commandtype'] == "changeauthrank":
+            #그룹 캘린더 사용자 조회
             useridcheck = usertable.objects.filter(userID=data['userid'], groupID=data['groupID'], caltype="Group")
+            #사용자 권한 조회
             auth3users = usertable.objects.filter(groupID=data['groupID'], authrank="3", caltype="Group")
             if useridcheck.count() == 0:
                 return HttpResponse("ID not exists")
@@ -159,6 +190,7 @@ def test(request):
                 sqldata = (data['authrank'], data['userid'], data['groupID'], 'Group')
                 conn.execute(sql, sqldata)
                 connectdb.commit()
+                #권한 변경
                 if auth3users.count() == 0:
                     sql = 'UPDATE mainserver_usertable SET authrank = :authrank WHERE userID = :userid AND' \
                           ' groupID = :groupID AND caltype = :group'
@@ -168,8 +200,11 @@ def test(request):
                     return HttpResponse("auth 3 user not exists")
                 else:
                     return HttpResponse("authrank change complete")
+        #사용자ID, 권한 조회
         elif data['commandtype'] == "searchcalendardata":
+            #캘린더ID 조회
             groupidcheck = usertable.objects.filter(groupID=data['groupID'])
+            #사용자 권한 조회
             userauthcheck = usertable.objects.filter(groupID=data['groupID'], userID=data['useridforcheck'], authrank="3")
             if groupidcheck.count() == 0:
                 return HttpResponse("ID not exists")
@@ -177,10 +212,12 @@ def test(request):
                 if(userauthcheck.count() == 0):
                     return HttpResponse("Authority error")
                 else:
+                    #사용자ID, 권한 조회
                     useridcheck = usertable.objects.filter(userID__contains=data['userid'], groupID=data['groupID'], caltype="Group"
                                                        ,authrank__contains=data['authrank'])
                     queryset = useridcheck.values_list()
                     jsonfile = []
+                    #사용자ID, 권한 JSON파일로 응답
                     for i in range(useridcheck.count()):
                         jsonfile.append({
                             "userid": queryset[i][1],
@@ -188,13 +225,17 @@ def test(request):
                         })
                     jsonparse = json.dumps({'items': jsonfile})
                     return HttpResponse(jsonparse)
+        #일정 삭제
         elif data['commandtype'] == "deleteschedule":
+            #사용자 권한 조회
             userauthcheck = usertable.objects.filter(groupID=data['groupID'], userID=data['userid'])
+            #삭제할 일정 조회
             scheduledelete = scheduletable.objects.get(userID=data['userid'], caltype=data['caltype'], groupID=data['groupID'],
                                            year=data['year'],
                                            month=data['month'], day=data['day'],
                                            hour=data['hour'], minute=data['minute'],
                                            content=data['content'])
+            #일정 리스트화
             queryset = userauthcheck.values_list()
             authquery = queryset[0][5]
             if authquery == "2" or authquery == "3":
@@ -202,7 +243,9 @@ def test(request):
                 return HttpResponse("delete complete")
             else:
                 return HttpResponse("Authority error")
+        #일정 변경
         elif data['commandtype'] == "updateschedule":
+            #변경 전 일정 검색
             userauthcheck = usertable.objects.filter(groupID=data['groupID'], userID=data['userid'])
             queryset = userauthcheck.values_list()
             authquery = queryset[0][5]
@@ -214,11 +257,13 @@ def test(request):
             else:
                 return HttpResponse("Authority error")
         elif data['commandtype'] == "updateschedule2":
+            #변경 전 일정
             currentdatacheck = scheduletable.objects.filter(userID=data['userid'], caltype=data['caltype'],
                                                     groupID=data['groupID'],
                                                     year=data['year'], month=data['month'], day=data['day'],
                                                     hour=updatefield[0], minute=updatefield[1], content=updatefield[2])
             if currentdatacheck.count() == 1:
+                #일정 변경
                 currentdata = scheduletable.objects.get(userID=data['userid'], caltype=data['caltype'],
                                                         groupID=data['groupID'],
                                                         year=data['year'], month=data['month'], day=data['day'],
@@ -232,14 +277,18 @@ def test(request):
                 return HttpResponse("update complete")
             updatefield.clear()
             return HttpResponse("no schedule")
+        #캘린더 불러오기
         elif data['commandtype'] == "importcalendar":
+            #사용자 권한 조회
             userauthcheck = usertable.objects.filter(groupID=data['groupID'], userID=data['userid'])
+            #그룹 캘린더 모든 일정 조회
             calendardata = scheduletable.objects.filter(groupID=data['groupID'], caltype=data['caltype'])
             queryset = userauthcheck.values_list()
             calendarqueryset = calendardata.values_list()
             jsonfile = []
             authquery = queryset[0][5]
             if authquery == "2" or authquery == "3":
+            #그룹 캘린더 JSON파일 응답
                 for i in range(calendardata.count()):
                     jsonfile.append({
                         "year": calendarqueryset[i][4],
